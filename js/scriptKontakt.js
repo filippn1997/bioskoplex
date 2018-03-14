7d59b981dbc3376caafff6a5509bcdaa87f71a72
filippn1997 <37354735+filippn1997@users.noreply.github.com>
2018-03-14 05:55:35 +0100

    Update scriptKontakt.js

--- js/scriptKontakt.js
+++ js/scriptKontakt.js
@@ -69,5 +69,9 @@ function resetuj()
     document.getElementById("prezime").value="";
     document.getElementById("email").value="";
     document.getElementById("textPolje").value="";
+    document.getElementById("textPoruka").style.visibility="hidden";
+    document.getElementById("emailPoruka").style.visibility="hidden";
+    document.getElementById("prezimePoruka").style.visibility="hidden";
+     document.getElementById("imePoruka").style.visibility="hidden";
 
 }
